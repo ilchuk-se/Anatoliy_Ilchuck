47f63fbe3df4c863ff817962837e246b444ad636
Anatolii Ilchuk <ilchuk.se@gmail.com>
2021-11-09 17:09:03 +0200

    Tasks 1-9 are fulfilled.

---
+++ src/main/java/com/epam/spring/homework1/config/OtherConfig.java
@@ -0,0 +1,25 @@
+package com.epam.spring.homework1.config;
+
+import com.epam.spring.homework1.beans.BeanA;
+import com.epam.spring.homework1.other.OtherBeanA;
+import com.epam.spring.homework1.other.OtherBeanB;
+import com.epam.spring.homework1.other.OtherBeanC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan("com.epam.spring.homework1.other")
+@Import(PetConfig.class)
+public class OtherConfig {
+    @Bean
+    public OtherBeanB otherBeanB(){
+        return new OtherBeanB();
+    }
+
+    @Bean
+    public OtherBeanC otherBeanC() {
+        return new OtherBeanC();
+    }
+}
